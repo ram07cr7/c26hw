689117dd2b6a13851d44438a25f3a8bda73deb6d
ram07cr7 <83637071+ram07cr7@users.noreply.github.com>
2021-06-06 21:41:28 +0530

    Add files via upload

---
+++ Ne folder (1)/collisionAlgorithm-master/sketch.js
@@ -0,0 +1,54 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  go1 =createSprite(250,400,20,20);
+  go1.shapeColor="green";
+
+  go2 =createSprite(450,400,20,20);
+  go2.shapeColor="green";
+
+  go3 =createSprite(350,400,20,20);
+  go3.shapeColor="green";
+
+  go4 =createSprite(550,400,20,20);
+  go4.shapeColor="green";
+
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+if(isTouching(movingRect,fixedRect)){
+
+
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+  drawSprites();
+}
+
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2) {
+   return true ;
+}
+else{
+  return false
+}
+}
